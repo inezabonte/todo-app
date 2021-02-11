a2a906907e5c26ed9b5d70aa0c29fcd830517129
inezabonte <inezabonte@gmail.com>
2021-02-12 01:14:31 +0200

    removed login, signup and authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
-import Login from './components/Login'
-import Signup from './components/Signup'
 import Todo from './components/Todo'
 import NavBar from './components/Navbar'
-import SuccessPage from './components/SuccessPage'
 
 function App (){
  
@@ -14,9 +11,6 @@ function App (){
         <Router>
             <Switch>
                 <Route path='/' exact component={Todo} />
-                <Route path='/login' exact component={Login} />
-                <Route path='/signup' exact component={Signup} />
-                <Route path='/success' exact component={SuccessPage} />
             </Switch>
         </Router>
        </> 
