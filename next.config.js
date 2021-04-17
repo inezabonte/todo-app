552a0c9a0a9683ff70b24796f47eac02da612480
inezabonte <inezabonte@gmail.com>
2021-04-17 19:23:34 +0200

    disabling pwa in development

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const withPWA = require('next-pwa')
+const prod = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public'
+    dest: 'public',
+    disable: prod ? false : true
   },
   future: {
     webpack5: true,
