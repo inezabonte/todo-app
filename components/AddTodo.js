4a216cf8827ff58e252cc9c1724a4fedeafa6e26
inezabonte <inezabonte@gmail.com>
2021-04-17 22:00:39 +0200

    Made some changes

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -13,7 +13,7 @@ function AddTodo(props){
     }
 
     return(
-        <Card title='Add Todo' bodyStyle={{paddingTop: '40px'}}>
+        <Card bodyStyle={{paddingTop: '40px'}}>
             <Form
             form={form}
             onFinish = {handleSubmition}
@@ -22,15 +22,15 @@ function AddTodo(props){
             className = "form"
             >
                 <Row gutter={16}>
-                    <Col xs={24} sm={24} md={16} lg={18} xl={20}>
+                    <Col xs={24} sm={24} md={16} lg={18} xl={18}>
                         <Form.Item 
                         name='task'
                         rules={[{ required: true, message: 'This field is required' }]}
                         >
-                            <Input placeholder='Create a task' allowClear />
+                            <Input placeholder='Create a task' allowClear/>
                         </Form.Item>
                     </Col>
-                    <Col xs={24} sm={24} md={8} lg={6} xl={4}>
+                    <Col xs={24} sm={24} md={8} lg={6} xl={6}>
                         <Button htmlType='submit' type='primary' icon={<PlusCircleFilled/>} block >Add Todo</Button>
                     </Col>
                 </Row>
